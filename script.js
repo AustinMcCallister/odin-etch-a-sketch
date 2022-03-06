90ab8de83cb5469d3edc1f3f251a8e499821a9bb
Austin McCallister <austin.mccallister@protonmail.com>
2022-03-06 00:33:05 -0500

    Add logic for dynamic generation of squares

---
+++ script.js
@@ -0,0 +1,12 @@
+const sketchBody = document.querySelector('.sketch-body');
+let gridSize = 16;
+
+for (let i = 0; i < gridSize; i++) {
+  for (let j = 0; j < gridSize; j++) {
+    const sketchSquare = document.createElement('div');
+    const squareSize = (sketchBody.clientWidth / gridSize);
+    sketchSquare.classList.add('sketch-square');
+    sketchSquare.setAttribute('style', `width: ${squareSize}px; height: ${squareSize}px;`)
+    sketchBody.appendChild(sketchSquare);
+  }
+}
