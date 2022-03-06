c172e3f1056ceb08c0fd0e0cf197f09bb88da79b
Austin McCallister <austin.mccallister@protonmail.com>
2022-03-06 12:27:31 -0500

    Add colorscheme array or hex codes

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const colorScheme = ['#2E3440', '#3B4252', '#434C5E', '#4C566A',
+                     '#ECEFF4', '#E5E9F0', '#D8DEE9', '#8FBCBB',
+                     '#88C0D0', '#81A1C1', '#5E81AC', '#BF616A',
+                     '#D08770', '#EBCB8B', '#A3BE8C', '#B48EAD'];
+
 const sketchBody = document.querySelector('.sketch-body');
 let gridSize = 16;
 
