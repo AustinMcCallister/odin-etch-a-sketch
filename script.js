44ef87d3028a5ce7f3de7fd32403887d4d5e64da
Austin McCallister <austin.mccallister@protonmail.com>
2022-03-06 13:39:37 -0500

    Add event listener for mouseover drawing

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const colorScheme = ['#2E3440', '#3B4252', '#434C5E', '#4C566A',
                      '#88C0D0', '#81A1C1', '#5E81AC', '#BF616A',
                      '#D08770', '#EBCB8B', '#A3BE8C', '#B48EAD'];
 
-const sketchBody = document.querySelector('.sketch-body');
 let gridSize = 16;
 
+const sketchBody = document.querySelector('.sketch-body');
 for (let i = 0; i < gridSize; i++) {
   for (let j = 0; j < gridSize; j++) {
     const sketchSquare = document.createElement('div');
@@ -15,3 +15,12 @@ for (let i = 0; i < gridSize; i++) {
     sketchBody.appendChild(sketchSquare);
   }
 }
+
+const sketchGrid = document.querySelectorAll('.sketch-square');
+sketchGrid.forEach((square) => {
+  square.addEventListener('mouseover', (event) => {
+    if (event.buttons === 1) {
+      event.target.style.backgroundColor = colorScheme[0];
+    }
+  });
+});
