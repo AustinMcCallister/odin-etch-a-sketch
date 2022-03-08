e6a9b565f2e5a71477de1ecaae9f2da8444930d1
Austin McCallister <austin.mccallister@protonmail.com>
2022-03-08 00:59:50 -0500

    Convert logic into functions and implement slider

--- script.js
+++ script.js
@@ -4,13 +4,15 @@ const colorScheme = ['#2E3440', '#3B4252', '#434C5E', '#4C566A',
                      '#D08770', '#EBCB8B', '#A3BE8C', '#B48EAD'];
 
 let gridSize = 16;
+drawGrid(gridSize);
+addGridEvents();
 
 const body = document.querySelector('body');
 body.setAttribute('ondragstart', 'return false;');
 
-const sketchBody = document.querySelector('.sketch-body');
-for (let i = 0; i < gridSize; i++) {
-  for (let j = 0; j < gridSize; j++) {
+function drawGrid(gridSize) {
+  const sketchBody = document.querySelector('.sketch-body');
+  for (let i = 0; i < gridSize ** 2; i++) {
     const sketchSquare = document.createElement('div');
     const squareSize = (sketchBody.clientWidth / gridSize);
     sketchSquare.classList.add('sketch-square');
@@ -19,21 +21,48 @@ for (let i = 0; i < gridSize; i++) {
   }
 }
 
-const sketchGrid = document.querySelectorAll('.sketch-square');
-sketchGrid.forEach((square) => {
-  square.addEventListener('mouseover', (event) => {
-    if (event.buttons === 1) {
-      event.target.style.backgroundColor = colorScheme[0];
-    }
-  });
-  square.addEventListener('mousedown', (event) => {
-    event.target.style.backgroundColor = colorScheme[0];
-  });
-});
-
-const resetButton = document.querySelector('#reset');
-resetButton.addEventListener('click', () => {
+function resetFunction() {
+  const sketchGrid = document.querySelectorAll('.sketch-square');
   sketchGrid.forEach((square) => {
     square.style.backgroundColor = colorScheme[6];
   });
-});
+}
+
+function deleteGrid() {
+  const gridSquares = document.getElementsByClassName('sketch-square');
+  const resetButton = document.querySelector('#reset');
+  resetButton.removeEventListener('click', resetFunction);
+  while(gridSquares.length > 0) {
+    gridSquares[0].parentNode.removeChild(gridSquares[0]);
+  }
+
+}
+
+function addGridEvents() {
+  const sketchGrid = document.querySelectorAll('.sketch-square');
+  const resetButton = document.querySelector('#reset');
+  resetButton.addEventListener('click', resetFunction);
+  sketchGrid.forEach((square) => {
+    square.addEventListener('mouseover', (event) => {
+      if (event.buttons === 1) {
+        event.target.style.backgroundColor = colorScheme[0];
+      }
+    });
+    square.addEventListener('mousedown', (event) => {
+      event.target.style.backgroundColor = colorScheme[0];
+    });
+  });
+}
+
+let slider = document.getElementById("grid-range");
+let output = document.getElementById("grid-value");
+output.innerHTML = `${slider.value} x ${slider.value}`; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = `${this.value} x ${this.value}`;
+  gridSize = this.value;
+  deleteGrid();
+  drawGrid(gridSize);
+  addGridEvents();
+}
